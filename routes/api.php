f4e2513b7562a91a113a579971d70140c096c385
alexgg9 <alexgalvezgarciaqt@gmail.com>
2025-02-10 09:38:25 +0100

    apiApplicationWorking

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\apiController\ApplicationApiController;
+
+/*
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+*/
+
+Route::resource('application', ApplicationApiController::class);
