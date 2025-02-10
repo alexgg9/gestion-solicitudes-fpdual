c9574cbdbd5702861860cf9cc42f46c7262a8731
alexgg9 <alexgalvezgarciaqt@gmail.com>
2025-02-10 11:06:58 +0100

    vistadashboard

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::resource('applications', ApplicationController::class);
 Route::resource('professors', ProfessorController::class);
 Route::resource('companies', CompanyController::class);
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
