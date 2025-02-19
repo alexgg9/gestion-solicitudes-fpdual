3bd84686586549fdc18dc80c1658a7157c5afef1
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2025-02-19 19:03:27 +0100

    professor test

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 
-class Professor extends Model
+
+class Professor extends Authenticatable
 {
 
     use HasApiTokens, HasFactory, Notifiable;
